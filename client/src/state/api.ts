b3dd35954980516e0a1d0a33678a4f9bdb0223b9
dmschrein <darrelmschreiner@gmail.com>
2024-09-20 13:37:58 -0700

    added dashboard homepage

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,3 +1,5 @@
+/* API service to handle API interactions in a declarative way */
+
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export interface Product {
@@ -57,14 +59,35 @@ export interface User {
 }
 
 export const api = createApi({
+  /* Sets up the base URL for the API */
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
-  reducerPath: "api",
+  reducerPath: "api" /* unique path to store the API state in Redux store */,
+  /* helps Redux Query manage cache invalidation by tagging resources. When a mutation (e.g., createProduct) occurs, the cache for certain tags can be invalidated*/
   tagTypes: ["DashboardMetrics", "Products", "Users", "Expenses"],
+
+  /* endpoints function where the actual API queries and mutations are defined 
+  Queries are used to fetch data from the server. Each query is defined by calling build.query() and specifying:
+  Type: The type of data the query will return.
+  Parameters: Input for the query (if any).
+  Query function: Defines the URL and any additional request parameters (e.g., search strings).
+
+  * getDashBoardMetrics Query:
+  * sends GET request to the /dashboard endpoint
+  * returns data of type DashboardMetrics
+  * providesTags: ["DashboardMetics"] tells Redux Query to cache the response and tag it under "DashboardMetrics". This tag can later be invalidated when related
+  * data is updated, like when a product is created or modified.
+  */
   endpoints: (build) => ({
     getDashboardMetrics: build.query<DashboardMetrics, void>({
       query: () => "/dashboard",
       providesTags: ["DashboardMetrics"],
     }),
+    /*
+     * The query sends a GET request to /products and optionally includes a search parameter.
+     * The response will be an array of Product[].
+     * If a search term is provided, it appends ?search=<search> as a query string to the URL.
+     * providesTags: ["Products"] caches the products and tags them under "Products" for potential invalidation later.
+     */
     getProducts: build.query<Product[], string | void>({
       query: (search) => ({
         url: "/products",
@@ -72,6 +95,13 @@ export const api = createApi({
       }),
       providesTags: ["Products"],
     }),
+
+    /*
+     * Mutations - are used to send data (e.g., POST, PUT, DELETE) to the server. After a mutation, certain tags are invalidated to refresh cached data.
+     * This mutation sends a POST request to /products with the newProduct object as the body.
+     * The response will return a Product object.
+     * invalidatesTags: ["Products"] ensures that after creating a new product, the cached products data is invalidated and refetched.
+     */
     createProduct: build.mutation<Product, NewProduct>({
       query: (newProduct) => ({
         url: "/products",
@@ -80,10 +110,16 @@ export const api = createApi({
       }),
       invalidatesTags: ["Products"],
     }),
+    /*
+     * This query sends a GET request to /users and expects an array of User[] objects in response.
+     */
     getUsers: build.query<User[], void>({
       query: () => "/users",
       providesTags: ["Users"],
     }),
+    /*
+     * The query sends a GET request to /expenses and expects an array of ExpenseByCategorySummary[]
+     */
     getExpensesByCategory: build.query<ExpenseByCategorySummary[], void>({
       query: () => "/expenses",
       providesTags: ["Expenses"],
@@ -91,6 +127,7 @@ export const api = createApi({
   }),
 });
 
+/* Export hooks */
 export const {
   useGetDashboardMetricsQuery,
   useGetProductsQuery,

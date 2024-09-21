10620d2d0863ff67916e2c40e5eaf9d97d74bb9d
dmschrein <darrelmschreiner@gmail.com>
2024-09-20 17:46:58 -0700

    products page and backend setup

--- client/src/app/[components]/Navbar/index.tsx
+++ client/src/app/[components]/Navbar/index.tsx
@@ -2,7 +2,7 @@
 
 import { useAppDispatch, useAppSelector } from "@/app/redux";
 import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
-import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
+import { Bell, Menu, Moon, SearchIcon, Settings, Sun } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -40,7 +40,7 @@ const Navbar = () => {
             className="pl-10 pr-r py-2 w-50 md:w-60 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
           />
           <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-non">
-            <Bell className="text-gray-500" size={20} />
+            <SearchIcon className="text-gray-500" size={20} />
           </div>
         </div>
       </div>
